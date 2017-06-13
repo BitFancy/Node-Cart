6b715939069294e80a30bd91ba1bb780a49dae36
fancydeveloper <alricbauer422031@gmail.com>
2017-06-13 16:43:38 +0200

    CHG: Installing morgan.js to monitor handled requests

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-
+const category = require("../category/route.js");
 app.use(morgan('tiny'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+app.use('/category', category);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
