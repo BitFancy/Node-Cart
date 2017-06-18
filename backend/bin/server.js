195896f52959173209e226ff9990883a479a9418
fancydeveloper <alricbauer422031@gmail.com>
2017-06-18 19:30:31 +0200

    CHG: introducig product route to server.js

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const category = require("../category/route.js");
-app.use(morgan('tiny'));
+const product = require("../app_modules/product/route.js");
 
-app.use('/category', category);
+
+app.use(morgan('tiny'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use('/product', product);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
